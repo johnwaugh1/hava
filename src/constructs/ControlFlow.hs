5b1a66e5c1c0ce54823498c5a824f0a6f7a4f1a7
John <waughjm@appstate.edu>
2024-12-28 14:39:27 -0500

    started making parser modules

--- src/constructs/ControlFlow.hs
+++ src/constructs/ControlFlow.hs
@@ -1,1 +1,12 @@
-module
+module ControlFlow (parseControlFlow) where
+
+import Lexer
+import Parser
+
+parseIfElse :: [Token] -> [Token] -> [Token]
+parseIfElse (Keyword "if" : LPar : PT condition : RPar : LBrace : body : RBrace : Keyword "else" : LBrace : elseBody : RBrace : s) q =
+    sr (If condition body elseBody : s) q
+
+parseWhileLoop :: [Token] -> [Token] -> [Token]
+parseWhileLoop (Keyword "while" : LPar : PT Condition : RPar : LBrace : body : RBrace : s) q =
+    sr (While condition body : s) q
