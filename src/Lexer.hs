155dbebfd064007fbda56f070c62fb71ddff15e2
John <waughjm@appstate.edu>
2024-12-28 11:58:09 -0500

    Updated lexer, implemented testing suite

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -1,3 +1,5 @@
+module Lexer where
+
 import Data.Char
 import Data.List
 
@@ -10,81 +12,107 @@ data Token = Keyword String
            | BoolLiteral Bool
            | Operator String
            | Symbol Char
-           | KeywordIf | KeywordElse | KeywordWhile
-           | KeywordReturn | KeywordInt | KeywordString
-           | KeywordPublic | KeywordPrivate | KeywordClass
-           | KeywordVoid | KeywordStatic | KeywordNew
-           | LPar | RPar | LBrace | RBrace | SemiColon | Comma
-           | Dot | Equals | Plus | Minus | Star | Slash
-           | And | Or | Not | GreaterThan | LessThan | GreaterThanEqual | LessThanEqual
-           | EqualEqual | NotEqual
+           | LPar | RPar | LBrace | RBrace | LBracket | RBracket
+           | SemiColon | Comma | Colon | QuestionMark | Dot 
+           | Equals | Plus | Minus | Star | Slash | Modulo
+           | And | Or | Not | GreaterThan | LessThan 
+           | GreaterThanEqual | LessThanEqual | EqualEqual | NotEqual
            | SingleLineComment String | MultiLineComment String
            | Err String
     deriving (Eq, Show)
 
+javaKeywords :: [String]
+javaKeywords = 
+    [ "abstract", "continue", "for", "new", "switch"
+    , "assert", "default", "goto", "package", "synchronized"
+    , "boolean", "do", "if", "private", "this"
+    , "break", "double", "implements", "protected", "throw"
+    , "byte", "else", "import", "public", "throws"
+    , "case", "enum", "instanceof", "return", "transient"
+    , "catch", "extends", "int", "short", "try"
+    , "char", "final", "interface", "static", "void"
+    , "class", "finally", "long", "strictfp", "volatile"
+    , "const", "float", "native", "super", "while"
+    ]
+
 -- Lexer Implementation
 lexer :: String -> [Token]
 lexer "" = []
--- Symbols and Punctuation
-lexer ('\\':xs) = Err "Invalid escape character" : lexer xs
+-- Whitespace
+lexer (x:xs) | isSpace x = lexer xs
+-- Symbols
 lexer ('(':xs) = LPar : lexer xs
 lexer (')':xs) = RPar : lexer xs
 lexer ('{':xs) = LBrace : lexer xs
 lexer ('}':xs) = RBrace : lexer xs
+lexer ('[':xs) = LBracket : lexer xs
+lexer (']':xs) = RBracket : lexer xs
 lexer (';':xs) = SemiColon : lexer xs
 lexer (',':xs) = Comma : lexer xs
 lexer ('.':xs) = Dot : lexer xs
+lexer (':':xs) = Colon : lexer xs
+lexer ('?':xs) = QuestionMark : lexer xs
+-- Operators
+lexer ('=':'=':xs) = EqualEqual : lexer xs
+lexer ('!':'=':xs) = NotEqual : lexer xs
+lexer ('>':'=':xs) = GreaterThanEqual : lexer xs
+lexer ('<':'=':xs) = LessThanEqual : lexer xs
+lexer ('&':'&':xs) = And : lexer xs
+lexer ('|':'|':xs) = Or : lexer xs
 lexer ('=':xs) = Equals : lexer xs
 lexer ('+':xs) = Plus : lexer xs
 lexer ('-':xs) = Minus : lexer xs
 lexer ('*':xs) = Star : lexer xs
 lexer ('/':xs) = Slash : lexer xs
--- Operators
-lexer ('&':'&':xs) = And : lexer xs
-lexer ('|':'|':xs) = Or : lexer xs
-lexer ('!':'=':xs) = NotEqual : lexer xs
-lexer ('=':'=':xs) = EqualEqual : lexer xs
-lexer ('>':'=':xs) = GreaterThanEqual : lexer xs
-lexer ('<':'=':xs) = LessThanEqual : lexer xs
+lexer ('%':xs) = Modulo : lexer xs
 lexer ('>':xs) = GreaterThan : lexer xs
 lexer ('<':xs) = LessThan : lexer xs
 lexer ('!':xs) = Not : lexer xs
--- Whitespace
-lexer (x:xs) | isSpace x = lexer xs
--- Keywords and Identifiers
-lexer (x:xs) | isAlpha x = let (ident, rest) = span isAlphaNum xs
-                           in case ident of
-                                "if"    -> KeywordIf : lexer rest
-                                "else"  -> KeywordElse : lexer rest
-                                "while" -> KeywordWhile : lexer rest
-                                "return" -> KeywordReturn : lexer rest
-                                "int"   -> KeywordInt : lexer rest
-                                "string" -> KeywordString : lexer rest
-                                "public" -> KeywordPublic : lexer rest
-                                "private" -> KeywordPrivate : lexer rest
-                                "class" -> KeywordClass : lexer rest
-                                "void"  -> KeywordVoid : lexer rest
-                                "static" -> KeywordStatic : lexer rest
-                                "new"   -> KeywordNew : lexer rest
-                                _       -> Identifier (x:ident) : lexer rest
--- Integers
-lexer (x:xs) | isDigit x = let (numStr, rest) = span isDigit xs
-                           in IntegerLiteral (read (x:numStr)) : lexer rest
--- Floating-Point Numbers
-lexer (x:xs) | isDigit x || x == '.' = let (numStr, rest) = span (\c -> isDigit c || c == '.') xs
-                                      in FloatingPointLiteral (read (x:numStr)) : lexer rest
+
+-- Single-line Comments
+lexer ('/':'/':xs) = 
+    let (comment, rest) = span (/= '\n') xs
+    in SingleLineComment comment : lexer rest
+
+-- Multi-line Comments
+lexer ('/':'*':xs) = parseMultiLineComment "" xs
+  where
+    parseMultiLineComment :: String -> String -> [Token]
+    parseMultiLineComment acc ('*':'/':ys) = MultiLineComment acc : lexer ys
+    parseMultiLineComment acc (y:ys) = parseMultiLineComment (acc ++ [y]) ys
+    parseMultiLineComment acc [] = [Err "Unterminated comment"]
+
 -- String Literals
-lexer ('"':xs) = let (str, rest) = span (/= '"') xs
-                 in StringLiteral str : lexer (tail rest)
--- Comments
-lexer ('/':'/':xs) = let (comment, rest) = span (/= '\n') xs
-                     in SingleLineComment comment : lexer rest
-lexer ('/':'*':xs) = let (comment, rest) = span (/= '*') xs
-                     in case rest of
-                        ('*':'/':ys) -> MultiLineComment comment : lexer ys
-                        _            -> Err "Unterminated comment" : lexer rest
+lexer ('"':xs) = parseStringLiteral xs
+  where
+    parseStringLiteral :: String -> [Token]
+    parseStringLiteral input = parse "" input
+      where
+        parse acc ('\\':'"':ys) = parse (acc ++ "\"") ys
+        parse acc ('\\':'\\':ys) = parse (acc ++ "\\") ys
+        parse acc ('"':ys) = StringLiteral acc : lexer ys
+        parse acc [] = [Err "Unterminated string literal"]
+        parse acc (y:ys) = parse (acc ++ [y]) ys
+
 -- Boolean Literals
-lexer (x:xs) | x == 't' = BoolLiteral True : lexer xs
-lexer (x:xs) | x == 'f' = BoolLiteral False : lexer xs
+lexer ('t':'r':'u':'e':xs) = BoolLiteral True : lexer xs
+lexer ('f':'a':'l':'s':'e':xs) = BoolLiteral False : lexer xs
+
+-- Floating-Point & Integer Literals
+lexer (x:xs) 
+    | isDigit x || x == '.' = 
+        let (numStr, rest) = span (\c -> isDigit c || c == '.') (x:xs)
+        in if '.' `elem` numStr
+            then FloatingPointLiteral (read numStr) : lexer rest
+            else IntegerLiteral (read numStr) : lexer rest
+
+-- Keywords and Identifiers
+lexer (x:xs) 
+    | isAlpha x || x == '_' = 
+        let (ident, rest) = span (\c -> isAlphaNum c || c == '_') (x:xs)
+        in if ident `elem` javaKeywords
+            then Keyword ident : lexer rest
+            else Identifier ident : lexer rest
+
 -- Invalid Characters
-lexer xs = [Err (take 10 xs)]
+lexer (x:xs) = Err ("Unrecognized token: " ++ [x]) : lexer xs
---
+++ test/LexerTests.hs
@@ -0,0 +1,95 @@
+import Lexer
+import Data.List (partition)
+
+type TestResult = (String, Bool)
+
+-- Run a single lexer test
+runTest :: String -> [Token] -> IO TestResult
+runTest input expected =
+    let result = lexer input
+    in if result == expected
+       then return (input, True)
+       else do
+           putStrLn $ "Test failed for input: " ++ show input ++ 
+                       "\n   Expected: " ++ show expected ++ 
+                       "\n   Actual:      " ++ show result
+           return (input, False)
+
+-- Remove duplicates in test output
+uniqueResults :: [TestResult] -> [TestResult]
+uniqueResults = foldr (\(input, res) acc -> if input `elem` map fst acc then acc else (input, res) : acc) []
+
+-- Run all tests
+main :: IO ()
+main = do
+    putStrLn "Running Lexer Tests...\n"
+
+    testResults <- sequence [
+        -- Keywords
+        runTest "int" [Keyword "int"],
+        runTest "if" [Keyword "if"],
+        runTest "return" [Keyword "return"],
+
+        -- Identifiers
+        runTest "variable" [Identifier "variable"],
+        runTest "_valid" [Identifier "_valid"],
+        runTest "var123" [Identifier "var123"],
+
+        -- Integer Literals
+        runTest "123" [IntegerLiteral 123],
+        runTest "0" [IntegerLiteral 0],
+
+        -- Floating-Point Literals
+        runTest "3.14" [FloatingPointLiteral 3.14],
+        runTest "0.0" [FloatingPointLiteral 0.0],
+
+        -- String Literals
+        runTest "\"hello\"" [StringLiteral "hello"],
+        runTest "\"escaped \\\"string\\\"\"" [StringLiteral "escaped \\\"string\\\""],
+        runTest "\"unterminated" [Err "Unterminated string literal"],
+
+        -- Boolean Literals
+        runTest "true" [BoolLiteral True],
+        runTest "false" [BoolLiteral False],
+
+        -- Operators
+        runTest "+" [Plus],
+        runTest "==" [EqualEqual],
+        runTest "!=" [NotEqual],
+
+        -- Symbols
+        runTest "(" [LPar],
+        runTest ")" [RPar],
+        runTest ";" [SemiColon],
+
+        -- Comments
+        runTest "// Single-line comment" [SingleLineComment " Single-line comment"],
+        runTest "/* Multi-line */" [MultiLineComment " Multi-line "],
+        runTest "/* Unterminated" [Err "Unterminated comment"],
+
+        -- Mixed Valid Code
+        runTest "int x = 5;" [Keyword "int", Identifier "x", Equals, IntegerLiteral 5, SemiColon],
+        runTest "if (x > 0) { return; }" 
+            [Keyword "if", LPar, Identifier "x", GreaterThan, IntegerLiteral 0, RPar,
+             LBrace, Keyword "return", SemiColon, RBrace],
+
+        -- Error Handling
+        runTest "#" [Err "Unrecognized token: #"],
+        runTest "$" [Err "Unrecognized token: $"]
+        ]
+
+    -- Ensure unique results
+    let uniqueTestResults = uniqueResults testResults
+    let (passedTests, failedTests) = partition snd uniqueTestResults
+
+    putStrLn "\nPassed Tests:"
+    mapM_ (putStrLn . (" - " ++) . fst) passedTests
+
+    putStrLn "\nFailed Tests:"
+    mapM_ (putStrLn . (" - " ++) . fst) failedTests
+
+    let totalTests = length uniqueTestResults
+    let passedCount = length passedTests
+    let failedCount = length failedTests
+
+    putStrLn $ "\nSummary: " ++ show passedCount ++ " / " ++ show totalTests ++ " tests passed."
